f233e86699c1ba73c5dcdd00783df80602f51f68
Liantsoa <liantsoarandrianasimbolarivelo@gmail.com>
2024-05-10 22:10:19 +0300

    ajouts des fichiers

---
+++ script.js
@@ -0,0 +1,5 @@
+// Exemple de fonctionnalité JavaScript
+document.querySelector('form').addEventListener('submit', function(event) {
+    event.preventDefault();
+    alert('Merci pour votre message!');
+});
