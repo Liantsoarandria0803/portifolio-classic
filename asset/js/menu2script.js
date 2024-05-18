0fede0a2db4dfcb9f0ab30540f5c05eccdd3b800
liantsoa <liantsoarandrianasimbolarivelo@gmail.com>
2024-05-18 19:47:21 +0300

    Remake modification

--- asset/js/menu2script.js
+++ asset/js/menu2script.js
@@ -3,7 +3,7 @@ document.getElementById("menu").onclick= function onOff(){
     console.log();
     if(x==4){
         x=x+1;
-        document.getElementById('navMenu').innerHTML= '<div class="navbar"><ul><li><a href="./../../index.html">Acceuil</a></li><li><a href="./about.html">A propos</a></li><li><a href="./service.html" >Projets</a></li><li><a href="./Contact.html" >Contact</a></li></div>';
+        document.getElementById('navMenu').innerHTML= '<div class="navbar"><a href="./../../index.html">Acceuil</a></br></br><a href="./about.html">A propos</a></br></br><a href="./service.html" >Projets</a></br></br><a href="./Contact.html" >Contact</a></div>';
     }
     else if(x==5){
         document.getElementById('navMenu').innerHTML= "";
--- asset/js/menuscript.js
+++ asset/js/menuscript.js
@@ -3,7 +3,7 @@ document.getElementById("menu").onclick= function onOff(){
     console.log();
     if(x==4){
         x=x+1;
-        document.getElementById('navMenu').innerHTML= '<div class="navbar"> <ul> <li><a href="./index.html">Acceuil</a></li><li><a href="./asset/html/about.html">A propos</a></li><li><a href="./asset/html/service.html">Projets</a></li><li><a href="./asset/html/Contact.html" >Contact</a></li></div>';
+        document.getElementById('navMenu').innerHTML= '<div class="navbar"><a href="./index.html">Acceuil</a></br></br><a href="./asset/html/about.html">A propos</a></br></br><a href="./asset/html/service.html">Projets</a></br></br><a href="./asset/html/Contact.html" >Contact</a></div>';
     }
     else if(x==5){
         document.getElementById('navMenu').innerHTML= "";
