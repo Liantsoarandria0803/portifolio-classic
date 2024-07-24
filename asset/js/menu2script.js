6b44c70cde25a8698b9bdad89cbe140ac96128d2
Liantsoa <liantsoarandrianasimbolarivelo@gmail.com>
2024-07-24 15:45:07 +0300

    ModificationJuly

--- asset/js/menu2script.js
+++ asset/js/menu2script.js
@@ -4,9 +4,11 @@ document.getElementById("menu").onclick= function onOff(){
     if(x==4){
         x=x+1;
         document.getElementById('navMenu').innerHTML= '<div class="navbar"><a href="./../../index.html">Acceuil</a></br></br><a href="./about.html">A propos</a></br></br><a href="./service.html" >Projets</a></br></br><a href="./Contact.html" >Contact</a></div>';
+        document.getElementById('menu').innerHTML="X"
     }
     else if(x==5){
         document.getElementById('navMenu').innerHTML= "";
+        document.getElementById('menu').innerHTML="&#9776"
         x=x-1;
     }
 } 
--- asset/js/menuscript.js
+++ asset/js/menuscript.js
@@ -4,9 +4,11 @@ document.getElementById("menu").onclick= function onOff(){
     if(x==4){
         x=x+1;
         document.getElementById('navMenu').innerHTML= '<div class="navbar"><a href="./index.html">Acceuil</a></br></br><a href="./asset/html/about.html">A propos</a></br></br><a href="./asset/html/service.html">Projets</a></br></br><a href="./asset/html/Contact.html" >Contact</a></div>';
+        document.getElementById('menu').innerHTML="X"
     }
     else if(x==5){
         document.getElementById('navMenu').innerHTML= "";
+        document.getElementById('menu').innerHTML="&#9776"
         x=x-1;
     }
 } 
